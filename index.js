bf4e2bd71ce70e8cdfd69639cba9322f43f3480b
Amit Singh <amit489@gmail.com>
2023-10-23 20:15:00 +0530

    QR code generator

---
+++ index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+
+
+$("button").on("click", buttClicked);
+        $(document).on("keydown", function(event) {
+            if (event.key === "Enter") {
+                buttClicked();
+            }
+        });
+
+        function buttClicked() {
+            var button1 = $("#text_url").val();
+            var qrNew = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data="+button1;
+            
+            $('#target').attr("src",qrNew);
+           
+        }
+    });
