2c193950a95111ab65c18b503ef6dcd899b7022d
Amit Singh <amit489@gmail.com>
2023-12-16 08:54:10 +0530

    chawla

--- index1.js
+++ index1.js
@@ -1,6 +1,6 @@
 var qr = require('qr-image');
  
-var qr_svg = qr.image('7627023295', { type: 'svg' });
+var qr_svg = qr.image('cszcsjhmn', { type: 'svg' });
 qr_svg.pipe(require('fs').createWriteStream('generated QR.svg'));
  
 var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
